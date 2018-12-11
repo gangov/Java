5b6dc2a0a4a621950093991e316d77ece053b625
Kaloyan Gangov <gangov@users.noreply.github.com>
2018-12-11 19:36:43 +0200

    2nd option
    added list, instead of using 2 maps, think it's better

--- MapsLambdaStreamAPI/src/com/company/ExerOrders.java
+++ MapsLambdaStreamAPI/src/com/company/ExerOrders.java
@@ -41,3 +41,47 @@ public class ExerOrders {
         }
     }
 }
+
+// second option - one map, with list
+package com.company;
+
+import java.util.*;
+
+public class TestingMaps {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        LinkedHashMap<String, List<Double>> items = new LinkedHashMap<String, List<Double>>();
+
+        double latestCount = 0;
+
+        while (true) {
+            String input = scan.nextLine();
+            if (input.equalsIgnoreCase("buy")) {
+                break;
+            }
+
+            String[] tokens = input.split(" ");
+            String name = tokens[0];
+            double price = Double.parseDouble(tokens[1]);
+            double quantity = Integer.parseInt(tokens[2]);
+
+            if (!items.containsKey(name)) {
+                items.put(name, new ArrayList<>());
+                items.get(name).add(price);
+                items.get(name).add(quantity);
+            } else {
+                double oldQuan = items.get(name).get(1);
+                items.get(name).remove(0);
+                items.get(name).add(0, price);
+                items.get(name).remove(1);
+                items.get(name).add(1, oldQuan + quantity);
+            }
+        }
+        
+        items.entrySet().stream()
+                .forEach(entry -> {
+                    System.out.printf("%s -> %.2f%n", entry.getKey(), entry.getValue().get(0) * entry.getValue().get(1));
+                });
+    }
+}
